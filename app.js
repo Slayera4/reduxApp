a0542d8a7a6138a4281a8f2bc2d1737019b244e2
Mauricio <slashera4@gmail.com>
2018-06-28 20:41:34 +0200

    CRUD operations API MONGODB

--- app.js
+++ app.js
@@ -21,27 +21,73 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/bookshop');
 Books = require('./models/books.js');
 //-----POST BOOKS----------
-app.post('/books', function(req, res){
- var book = req.body;
- Books.create(book, function(err, books){
- if(err){
- throw err;
- }
- res.json(books);
- })
+app.post('/books', function (req, res) {
+  var book = req.body;
+  Books.create(book, function (err, books) {
+    if (err) {
+      throw err;
+    }
+    res.json(books);
+  })
 });
+
+//------->>> GET BOOKS <<<-------
+app.get('/books', function (req, res) {
+  Books.find(function (err, books) {
+    if (err) {
+      throw err;
+    }
+    res.json(books);
+  })
+});
+
+//---->>> DELETE BOOKS <<<------
+app.delete('/books/:_id', function (req, res) {
+  var query = { _id: req.params._id };
+  Books.remove(query, function (err, books) {
+    if (err) {
+      throw err;
+    }
+    res.json(books);
+  })
+});
+
+//---->>> UPDATE BOOKS <<<------
+app.put('/books/:_id', function (req, res) {
+  var book = req.body;
+  var query = { _id: req.params._id };
+  // if the field doesn't exist $set will set
+  //a new field
+  var update = {
+    '$set': {
+      title: book.title,
+      description: book.description,
+      image: book.image,
+      price: book.price
+    }
+  };
+  // When true returns the updated document
+  var options = { new: true };
+  Books.findOneAndUpdate(query, update,
+    options, function (err, books) {
+      if (err) {
+        throw err;
+      }
+      res.json(books);
+    })
+})
 //END APIs
-app.get('*', function(req,res){
+app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
